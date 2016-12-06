770f1bdf4a09d61ef18357f2f7868512817d5a02
cbpark <cbpark@gmail.com>
2016-12-06 17:06:03 +0900

    Finish ImPure

--- ImPure/DoLetExample2.hs
+++ ImPure/DoLetExample2.hs
@@ -1,15 +1,18 @@
 module DoLetExample2 where
-  
-example1 = do
-  putStrLn "Enter an integer number:"
-  s <- getLine
-  let number = (read s :: Int) + 2
-  putStrLn $ "Number plus 2 = " ++ (show number)
 
+example1 :: IO ()
+example1 = do
+    putStrLn "Enter an integer number:"
+    s <- getLine
+    let number = (read s :: Int) + 2
+    putStrLn $ "Number plus 2 = " ++ show number
+
+example2 :: IO ()
 example2 = do
   putStrLn "Enter an integer number:"
   s <- getLine
   let number = (read s :: Int) + 2 in
-    putStrLn $ "Number plus 2 = " ++ (show number)
+      putStrLn $ "Number plus 2 = " ++ show number
 
+main :: IO ()
 main = example1 >> example2 >> example1
--- ImPure/DoLetExample3.hs
+++ ImPure/DoLetExample3.hs
@@ -1,9 +1,12 @@
 module DoLetExample3 where
-  
-example3 =  putStrLn "Enter an integer number:" >>  getLine
 
+example3 :: IO String
+example3 = putStrLn "Enter an integer number:" >> getLine
+
+example4 :: String -> IO ()
 example4 mv = do
-  let number = (read mv :: Int) + 2
-  putStrLn $ "Number plus 2 = " ++ (show number)
+    let number = (read mv :: Int) + 2
+    putStrLn $ "Number plus 2 = " ++ show number
 
+main :: IO ()
 main = example3 >>= example4
