e216fe04abd79a85c4ab7a85b8102f4c57c5b625
cbpark <cbpark@gmail.com>
2016-12-06 22:06:47 +0900

    Finish ImPure (2)

--- ImPure/FmapExample.hs
+++ ImPure/FmapExample.hs
@@ -1,13 +1,13 @@
 module FmapExample where
 
+fileToWords :: FilePath -> IO [String]
 fileToWords fileName = do
-  fileText <- readFile fileName
-  return $ words fileText
-    
+    fileText <- readFile fileName
+    return $ words fileText
+
+main :: IO ()
 main = do
-  words1 <- fileToWords "text1.txt"
-  print $ reverse words1
-  words2 <- fmap reverse $ fileToWords "text1.txt"
-  print words2
-    
-  
+    words1 <- fileToWords "text1.txt"
+    print $ reverse words1
+    words2 <- fmap reverse (fileToWords "text1.txt")
+    print words2
--- StateMonad/State1.hs
+++ StateMonad/State1.hs
@@ -4,22 +4,23 @@ import Control.Monad.State
 
 incrementState :: State Int Int
 incrementState = do
-  n <- get
-  put (n + 1)
-  return n
+    n <- get
+    put (n + 1)
+    return n
 
 -- same state monad without using a 'do' expression:
 incrementState2 :: State Int Int
 incrementState2 = get >>= \a ->
-                  put (a + 1) >>= \b ->
+                  put (a + 1) >>= \_ ->
                   return a
 
-bumpVals (a,b) = (a+1, b+2)
+bumpVals :: (Int, Int) -> (Int, Int)
+bumpVals (a, b) = (a + 1, b + 2)
 
+main :: IO ()
 main = do
-  print $ runState incrementState 1  -- (1,2) == (return value, final state)
-  print $ runState incrementState2 1 -- (1,2) == (return value, final state)
-  print $ runState (mapState bumpVals incrementState) 1 -- (2,4)
-  print $ evalState incrementState 1  -- 1 == return value
-  print $ execState incrementState 1  -- 2 == final state
-
+    print $ runState incrementState 1  -- (1,2) == (return value, final state)
+    print $ runState incrementState2 1 -- (1,2) == (return value, final state)
+    print $ runState (mapState bumpVals incrementState) 1 -- (2,4)
+    print $ evalState incrementState 1  -- 1 == return value
+    print $ execState incrementState 1  -- 2 == final state
